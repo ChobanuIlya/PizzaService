16140825c2eed911a7ce011847e33a1962d3506b
Abraham <chobanu.ilya@icloud.com>
2022-04-03 19:11:44 +0300

    Refactor CustomerRepository

--- src/main/java/com/example/pizzaservice/repository/CustomerRepository.java
+++ src/main/java/com/example/pizzaservice/repository/CustomerRepository.java
@@ -1,8 +1,10 @@
 package com.example.pizzaservice.repository;
 
+import com.example.pizzaservice.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository {
+    Customer findByPhonenumber(String phoneNumber);
 }
