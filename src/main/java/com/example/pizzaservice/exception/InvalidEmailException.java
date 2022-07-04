2b7a223389153f03b59a41bc3a2593cdb8c24199
Abraham <chobanu.ilya@icloud.com>
2022-07-04 23:59:15 +0300

    Deleting unused classes

--- src/main/java/com/example/pizzaservice/exception/InvalidEmailException.java
+++
@@ -1,9 +0,0 @@
-package com.example.pizzaservice.exception;
-
-public class InvalidEmailException extends RuntimeException{
-    public InvalidEmailException(String errorMessage, Throwable err) {
-        super(errorMessage, err);
-    }
-
-    public InvalidEmailException(String errorMessage){}
-}
