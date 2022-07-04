c291ed9c07de537ad3c1f4f2e5a9d9600a3520b1
Abraham <chobanu.ilya@icloud.com>
2022-07-05 00:00:13 +0300

    Adding methods to repository

--- src/main/java/com/example/pizzaservice/repository/OrderRepository.java
+++ src/main/java/com/example/pizzaservice/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.example.pizzaservice.repository;
 
 import com.example.pizzaservice.entity.Order;
+import com.example.pizzaservice.entity.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
